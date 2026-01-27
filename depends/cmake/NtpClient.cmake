26f81002299ada3e84a50598493491295531da5f
Cross425 <asasin12335@gmail.com>
2026-01-27 23:49:54 +0300

    Add files via upload

--- depends/cmake/NtpClient.cmake
+++ depends/cmake/NtpClient.cmake
@@ -1,6 +1,20 @@
+# NtpClient.cmake
+# Patched for OpenWrt/offline builds
+#
+# When FPTN_NO_FETCHCONTENT=ON, do NOT download anything.
+# This prevents FetchContent from accessing the network in CI/OpenWrt.
+
+if(FPTN_NO_FETCHCONTENT)
+  message(STATUS "FPTN_NO_FETCHCONTENT=ON: skipping NTP-client FetchContent")
+  return()
+endif()
+
 include(FetchContent)
 
-FetchContent_Declare(ntp_client URL https://github.com/batchar2/NTP-client/archive/refs/heads/master.zip)
+FetchContent_Declare(
+  ntp_client
+  URL https://github.com/batchar2/NTP-client/archive/refs/heads/master.zip
+)
 
 FetchContent_GetProperties(ntp_client)
 if(NOT ntp_client_POPULATED)
